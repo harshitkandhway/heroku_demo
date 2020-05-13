bd218f7c659c5175aa8ce3a426bbf5bb12e11095
Harshit Kandhway <kandhway.harshit198@gmail.com>
2020-05-13 08:43:10 +0530

    initial commit -project setup

---
+++ src/main/java/com/hk/herokudemo/HerokudemoApplication.java
@@ -0,0 +1,11 @@
+package com.hk.herokudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokudemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HerokudemoApplication.class, args);
+	}
+}
